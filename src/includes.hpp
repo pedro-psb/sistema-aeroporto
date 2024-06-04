bae8484221f6a32f22a04404cfe98c357936ba2a
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-06-04 18:34:21 -0300

    :construction: feat: add file with includes

---
+++ src/includes.hpp
@@ -0,0 +1,19 @@
+// Arquivo de cabeçalho "INCLUDES.hpp"
+#ifndef INCLUDES_HPP
+#define INCLUDES_HPP
+
+#include <string>
+#include <iostream>
+
+#include "Flight.hpp"
+#include "Person.hpp"
+#include "Steward.hpp"
+#include "Ticket.hpp"
+#include "DateTime.hpp"
+#include "Destination.hpp"
+#include "Passenger.hpp"
+// #include "../EnumFlight.hpp"
+// #include "../EnumSeat.hpp"
+
+
+#endif // INCLUDES_HPP
