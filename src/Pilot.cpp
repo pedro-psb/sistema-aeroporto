385449ce08b119c3fa060c9a2bc260b260d1f32a
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-06-09 16:18:26 -0300

    :construction: feat: create Pilot

---
+++ src/Pilot.cpp
@@ -0,0 +1,8 @@
+#include "Pilot.hpp"
+
+Pilot::Pilot(string name, string cpf, int license):
+    Person(name, cpf), license(license)
+{
+    
+}
+    
--- src/includes.hpp
+++ src/includes.hpp
@@ -12,6 +12,7 @@
 #include "DateTime.hpp"
 #include "Destination.hpp"
 #include "Client.hpp"
+#include "Pilot.hpp"
 #include "Enum/EnumFlight.hpp"
 #include "Enum/EnumSeat.hpp"
 
