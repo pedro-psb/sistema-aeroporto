8b2bbb9cd192585d13014bcf2f3aab879acd8623
Pedro Brochado <pedropsb95@gmail.com>
2024-06-15 03:44:43 -0300

    FlightSchedule: remove comments and run clang-format

--- src/FlightSchedule.cpp
+++ src/FlightSchedule.cpp
@@ -1,57 +1,32 @@
 #include "FlightSchedule.hpp"
 #include <algorithm>
-FlightSchedule::FlightSchedule(){
-    flights = vector<Flight*>();//cria o vetor vazio
+
+FlightSchedule::FlightSchedule() {
+  flights = vector<Flight *>(); // cria o vetor vazio
 }
-FlightSchedule::FlightSchedule(vector<Flight*> flights) : flights(flights) {
- /*   if (numFlights > 0) {//verifica se o nÃºmero Ã© maior que zero, para criar o vetor de voos
-        this->flights = new Flight[numFlights];
-        memcpy(this->flights, flights, numFlights * sizeof(Flight)); //para mais informaÃ§Ãµes da funÃ§Ã£o: https://www.geeksforgeeks.org/memcpy-in-cc/
-    } else {//caso seja menor ou igual a zero, retorna um ponteiro nulo
-        this->flights = nullptr;
-    }*/
+
+FlightSchedule::FlightSchedule(vector<Flight *> flights) : flights(flights) {}
+
+vector<Flight *> FlightSchedule::getFlights() { return flights; }
+
+void FlightSchedule::addFlight(vector<Flight *> Flights) {
+  flights.insert(flights.end(), Flights.begin(), Flights.end());
 }
-vector<Flight*> FlightSchedule::getFlights(){
-    return flights;
+
+void FlightSchedule::addFlight(Flight *flight) { flights.push_back(flight); }
+
+void FlightSchedule::removeFlight(Flight *flight) {
+  auto it = std::find(flights.begin(), flights.end(), flight);
+  if (it != flights.end()) {
+    flights.erase(it);
+  }
 }
-/*void FlightSchedule::setFlights(Flight* flights, int numFlights) {
-    delete[] this->flights; //limpa a memoria de flights, caso haja
-    this->numFlights = numFlights;
 
-    if (numFlights > 0) {//se o nÃºmero de voos, for maior que zero, gerarÃ¡ o vetor com os voos
-        this->flights = new Flight[numFlights];
-        memcpy(this->flights, flights, numFlights * sizeof(Flight));
-    } else {
-        this->flights = nullptr;
-    }
-}*/
-        /* Add a vector of flights all at once. */
-        void FlightSchedule::addFlight(vector<Flight*> Flights){
-            flights.insert(flights.end(), Flights.begin(), Flights.end());
-        }
+int FlightSchedule::count() { return flights.size(); }
 
-        /* Add a single flight. */
-        void FlightSchedule::addFlight(Flight* flight){
-            flights.push_back(flight);
-        }
-
-        /* Remove a single flight. */
-        void FlightSchedule::removeFlight(Flight* flight){
-            auto it = std::find(flights.begin(), flights.end(), flight);
-                if (it != flights.end()) {
-                    flights.erase(it);
-                }
-        }
-
-        /* Get number of flights */
-        int FlightSchedule::count(){
-            return flights.size();
-        }
-
-        /* Clear flights table */
-        int FlightSchedule::clearFlights(){
-            int size;
-            size=flights.size();
-            flights.clear();
-            return size;
-        }
+int FlightSchedule::clearFlights() {
+  int size;
+  size = flights.size();
+  flights.clear();
+  return size;
+}
